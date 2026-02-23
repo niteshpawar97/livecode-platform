78dbc72dd2016742a355adacba7d56cf0203d3cb
niteshpawar97 <niteshpawar97@gmail.com>
2026-02-23 19:23:04 +0530

    production error fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { loader } from '@monaco-editor/react';
+import * as monaco from 'monaco-editor';
 import App from './App.jsx';
 import './App.css';
+
+// Use locally bundled Monaco instead of CDN (required for CSP in production)
+loader.config({ monaco });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
